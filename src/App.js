522a2feb0ae49c94ba4e28682ccdce324612ac6e
vamsi krish chiguruwada <vamsikrishchiguruwada@gmail.com>
2022-11-29 03:44:00 +0530

    testq

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/portfolio2.0" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index path="/portfolio2.0" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
