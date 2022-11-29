a99b10b45f3a214dba79ab0cf00891008660757f
Vamsi krish chiguruwada <vamsikrishchiguruwada@gmail.com>
2022-11-30 00:04:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/Portfolio">
+    <BrowserRouter >
       <App />
-    </Router>
+    </BrowserRouter>
     ,
   </React.StrictMode>,
   document.getElementById('root')
